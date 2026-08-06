a10ec3dd924d9cdb0b40cd7cf964b44fe9c35a64
dinellipaulo <pgsd1453@gmail.com>
2026-08-06 13:28:45 -0400

    update: changes at error handling in service layer

--- src/services/teamService.ts
+++ src/services/teamService.ts
@@ -1,11 +1,11 @@
 import { prisma } from "../lib/prisma/prisma.js";
-import { Team } from "../lib/prisma/generated/prisma/client.js";
+import { Prisma, Team } from "../lib/prisma/generated/prisma/client.js";
 
 import RequestParams from '../utils/interfaces/RequestParams.js'
 import TeamBodyProps from "../utils/interfaces/TeamBodyProps.js";
 
-export async function createTeamService({ id, name, basedAt, createdAt }: Team): Promise<Team> {
-    
+export async function createTeamService({ id, name, basedAt, createdAt }: Team): Promise<Team | null> {
+
     const teamExists = await prisma.team.findFirst({
         where: {
             name,
@@ -13,8 +13,8 @@ export async function createTeamService({ id, name, basedAt, createdAt }: Team):
         }
     });
 
-    if(teamExists) throw new Error("Team already exists");
-    
+    if (teamExists) return null;
+
     const newTeam = prisma.team.create({
         data: {
             id,
@@ -34,45 +34,62 @@ export async function getAllTeamsService() {
     return data;
 }
 
-export async function findTeamByIdService({ id }: RequestParams): Promise<Team> {
+export async function findTeamByIdService({ id }: RequestParams): Promise<Team | null> {
     const team = await prisma.team.findFirst({
         where: {
             id,
         }
     });
 
-    if(!team) throw new Error("Unable to find team");
+    if (!team) return null;
 
     return team;
 }
 
-export async function updateTeamService({ id }: RequestParams, { name, basedAt }: TeamBodyProps): Promise<Team> {
-    const teamToUpdate = await prisma.team.update({
-        where: {
-            id
-        }, 
-        data: {
-            name,
-            basedAt
-        }
-    });
+export async function updateTeamService({ id }: RequestParams, { name, basedAt }: TeamBodyProps): Promise<Team | null> {
+    try {
+        const teamToUpdate = await prisma.team.update({
+            where: {
+                id
+            },
+            data: {
+                name,
+                basedAt
+            }
+        });
 
-    if(!teamToUpdate) throw new Error("Unable to update team");
+        if (!teamToUpdate) return null;
 
-    return teamToUpdate;
+        return teamToUpdate;
+    } catch (error) {
+        // Prisma sends a specific error when it doesn't find data to delete, which prevents the server from sending http status codes
+        // The following line treats Prisma's error code, which lets the server send the correct http status as response
+        if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2025') return null;
+
+        return null;
+    }
 }
 
-export async function deleteTeamService({id}: RequestParams): Promise<Team> {
-    
-    if(!id) throw new Error("Unable to delete");
-    
-    const dataToDelete = await prisma.team.delete({
-        where: {
-            id
-        }
-    });
+export async function deleteTeamService({ id }: RequestParams): Promise<Team | null> {
 
-    if(!dataToDelete) throw new Error("Unable to delete");
+    try {
+        if (!id) throw new Error("Unable to delete");
 
-    return dataToDelete;
+        const dataToDelete = await prisma.team.delete({
+            where: {
+                id
+            }
+        });
+
+        if (!dataToDelete) return null;
+
+        return dataToDelete;
+    } catch (error) {
+        // Prisma sends a specific error when it doesn't find data to delete, which prevents the server from sending http status codes
+        // The following line treats Prisma's error code, which lets the server send the correct http status as response
+        if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2025') return null;
+
+        return null;
+    }
+
 }
